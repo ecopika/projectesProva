1a9c84926563b1f7a4ce143973f99f6e7ddeee67
MNN <MNN@MNN-PC>
2014-02-07 18:40:05 +0100

    V.NUSS.1

--- CompartPro/src/uf2/comp/Compartir.java
+++ CompartPro/src/uf2/comp/Compartir.java
@@ -5,6 +5,8 @@ public class Compartir {
 	public static void main(String[] args){
 		System.out.println("Hola com va?");
 		System.out.println("Hey hola companys, soc en Nuss!");
+		System.out.println("Aixo no rutlla");
+		
 
 	}
 
