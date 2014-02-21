eb42017a4f5666b045dc192234b8c706b70a01cf
ecopika <ecopika@ecopika-Aspire-5732Z>
2014-02-21 18:37:11 +0100

    v12

---
+++ CompartPro/src/uf2/comp/Polinomi2nGrau.java
@@ -0,0 +1,14 @@
+package uf2.comp;
+
+public class Polinomi2nGrau {
+	private int a;
+	private int b;
+	private int c;
+	
+	public Polinomi2nGrau(int a, int b, int c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+}
