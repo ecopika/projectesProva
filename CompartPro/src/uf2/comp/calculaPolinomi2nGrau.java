1f23a66dc37bf1fa3818a8a9ead3777df0b6c678
gerard <gerard@gerard-K55A>
2014-02-21 19:42:30 +0100

    v6

---
+++ CompartPro/src/uf2/comp/calculaPolinomi2nGrau.java
@@ -0,0 +1,23 @@
+package uf2.comp;
+
+import java.util.Scanner;
+
+public class calculaPolinomi2nGrau {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int a,b,c;
+		
+		System.out.println("Introdueix a");
+		a=sc.nextInt();
+		
+		System.out.println("Intordueix b");
+		b=sc.nextInt();
+		
+		System.out.println("Introdueix c");
+		c=sc.nextInt();
+
+	}
+
+}
