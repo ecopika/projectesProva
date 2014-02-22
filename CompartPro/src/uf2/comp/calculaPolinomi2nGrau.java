7d55e6b63acb21000827f1d06e951d3df0a63d91
ecopika <ecopika@ecopika-Aspire-5732Z>
2014-02-22 17:39:36 +0100

    sad

--- CompartPro/src/uf2/comp/calculaPolinomi2nGrau.java
+++ CompartPro/src/uf2/comp/calculaPolinomi2nGrau.java
@@ -6,13 +6,13 @@ public class calculaPolinomi2nGrau {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		Polinomi2nGrau pol = new Polinomi2nGrau(12, 2, 4);
+		Polinomi2nGrau pol = new Polinomi2nGrau(12, 2, -10);
 		int a,b,c;
 		
 		System.out.println(pol.toString());
 		System.out.println("Nombre de solucions:"+pol.nombreSolucions());
-		pol.solucio1();
-		pol.solucio2();
+		System.out.println("Sol·lució 1:"+pol.solucio1());
+		System.out.println("Sol·lució 2:"+pol.solucio2());
 		System.out.println("Introdueix a");
 		a=sc.nextInt();
 		
